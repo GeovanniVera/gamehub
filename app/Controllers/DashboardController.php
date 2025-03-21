3df7622172b8dd63b059b0a937664cfa5158a231
geovanni vera <geovanni.vera23@gmail.com>
2025-03-21 13:44:45 -0600

    se agrego el inicio de sesion

---
+++ app/Controllers/DashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class DashboardController extends BaseController
+{
+
+
+    protected static function validateData($data) {}
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../app/Classes/app.php';
 
 use App\Controllers\AuthController;
 use App\Controllers\RegisterController;
+use App\Controllers\DashboardController;
+
 use MVC\Router;
 
 $router = new Router();
@@ -13,15 +15,12 @@ $router->get('/',[AuthController::class,'login']);
 $router->post('/',[AuthController::class,'loginProcess']);
 $router->get('/logout',[AuthController::class,'logout']);
 
-//Recuperar password
-$router->get('/forgetPassword',[AuthController::class,'forgetPassword']);
-$router->post('/forgetPassword',[AuthController::class,'forgetPassword']);
-$router->get('/recoveryPassword',[AuthController::class,'recoveryPassword']);
-$router->post('/recoveryPassword',[AuthController::class,'recoveryPassword']);
 
 //Crear Cuenta
 $router->get('/register',[RegisterController::class,'register']);
 $router->post('/register',[RegisterController::class,'saveUser']);
 
+//Dashboard 
+$router->get('/dashboard',[DashboardController::class,'index']);
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
